746fedf2eabefe8775b1a38826f0372dacac19a8
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 06:29:46 +0000

    Switched to a dummy project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from 'vue';
 
-createApp(App).use(store).mount('#app')
+import App from './App.vue';
+
+createApp(App).mount('#app');
